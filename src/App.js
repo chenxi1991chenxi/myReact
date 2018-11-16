bb27825b4d729da93b7ac26d37fb0ebc36a42c88
wanchenxi <wanchenxi@genomics.cn>
2018-11-16 13:55:41 +0800

    搭建react脚手架

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+    constructor(props) { //构造函数
+      super(props);
+      this.state = {
+        text: 'This is TEXT!'
+      }
+    }
+    render() {
+      return ( <div> {this.state.text} </div>)
+      }
+    }
+    export default Ap
